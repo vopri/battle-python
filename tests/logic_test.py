cec91acecaae676e34c7b7870579bc4fde072a0b
Volker <volker.prior@gmail.com>
2022-03-06 12:25:25 +0100

    Add some tests

---
+++ tests/logic_test.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+import pytest
+from battle_snake import logic
+from battle_snake.entities import NextStep
+from pytest import fixture
+
+
+@fixture
+def move_request_head_origin():
+    move_request_1 = Path.cwd() / "tests" / "move_request_head_origin.json"
+    return json.loads(move_request_1.read_text())
+
+
+@pytest.mark.repeat(50)
+def test_path(move_request_head_origin):
+    chosen_move = logic.choose_move(move_request_head_origin)
+    assert chosen_move != NextStep.DOWN.value
+    assert chosen_move != NextStep.LEFT.value
