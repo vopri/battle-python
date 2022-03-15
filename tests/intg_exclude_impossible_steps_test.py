c482d7d3437591a66808d6e076cb2f822f8ee8e6
Volker <volker.prior@gmail.com>
2022-03-15 21:56:53 +0100

    Add some test cases

---
+++ tests/intg_exclude_impossible_steps_test.py
@@ -0,0 +1,74 @@
+import pytest
+from battle_snake.entities import NextStep, Snake
+from battle_snake.interactor import MoveDecision
+
+
+@pytest.mark.parametrize(
+    "snake_fix_str, expected_steps",
+    [
+        (
+            "snake_origin",
+            {NextStep.RIGHT},
+        ),
+        (
+            "snake_in_block",
+            {NextStep.RIGHT, NextStep.DOWN},
+        ),
+        (
+            "snake_top_middle_short",
+            {NextStep.LEFT, NextStep.DOWN},
+        ),
+        (
+            "snake_bottom_middle_short",
+            {NextStep.LEFT, NextStep.UP},
+        ),
+        (
+            "snake_left_middle_short",
+            {NextStep.DOWN, NextStep.UP},
+        ),
+        (
+            "snake_right_middle_short",
+            {NextStep.DOWN, NextStep.UP},
+        ),
+        (
+            "snake_top_right_short",
+            {NextStep.LEFT},
+        ),
+        (
+            "snake_top_right_short_collision_same_size",
+            {NextStep.DOWN},
+        ),
+        (
+            "snake_long",
+            {NextStep.UP, NextStep.LEFT, NextStep.RIGHT},
+        ),
+        (
+            "snake_long_2",
+            {NextStep.UP, NextStep.LEFT, NextStep.DOWN},
+        ),
+        (
+            "snake_long_2_future_down_without_food",
+            {NextStep.LEFT, NextStep.DOWN},
+        ),
+    ],
+)
+def test_exclude_impossible_steps(
+    sample_move_decision: MoveDecision,
+    snake_fix_str: str,
+    expected_steps: set[NextStep],
+    request: pytest.FixtureRequest,
+):
+    _arrange_test_exclude_impossible_steps(sample_move_decision, snake_fix_str, request)
+    sample_move_decision._exclude_impossible_moves()
+    assert set(sample_move_decision.possible_moves.values()) == expected_steps
+
+
+def _arrange_test_exclude_impossible_steps(
+    sample_move_decision, snake_fix_str, request
+):
+    snake: Snake = request.getfixturevalue(snake_fix_str)
+    sample_move_decision.board.all_snakes = {snake.head: snake}
+    sample_move_decision.board.my_head = snake.head
+    sample_move_decision.possible_moves = (
+        sample_move_decision.me.next_theoretical_head_positions_and_moves()
+    )
